4bfd42faaaba2de2ba40a837371cf584a5b53fcb
Smoked Fish <espy.jacob@gmail.com>
2024-04-05 12:13:28 -0700

    init

---
+++ ModEntry.cs
@@ -0,0 +1,72 @@
+﻿using BetterBuildMenu.Framework.Managers;
+using HarmonyLib;
+using StardewModdingAPI.Events;
+using StardewModdingAPI;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BetterBuildMenu
+{
+    public class ModEntry : Mod
+    {
+        // Shared static helpers
+        internal static IMonitor monitor;
+        internal static IModHelper modHelper;
+        internal static Multiplayer multiplayer;
+        internal static ModConfig modConfig;
+
+        // Managers
+        internal static ApiManager apiManager;
+
+        public override void Entry(IModHelper helper)
+        {
+            // Setup i18n
+            I18n.Init(helper.Translation);
+
+            // Setup the monitor, helper and multiplayer
+            monitor = Monitor;
+            modHelper = helper;
+            multiplayer = helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue();
+
+            // Setup the manager
+            apiManager = new ApiManager(monitor);
+
+            // Load the Harmony patches
+            try
+            {
+                var harmony = new Harmony(this.ModManifest.UniqueID);
+
+                // Apply GameLocation patches
+                // new ExamplePatch(monitor, helper).Apply(harmony);
+
+            }
+            catch (Exception e)
+            {
+                Monitor.Log($"Issue with Harmony patching: {e}", LogLevel.Error);
+                return;
+            }
+
+            // Hook into GameLoop events
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            modConfig = Helper.ReadConfig<ModConfig>();
+
+            if (Helper.ModRegistry.IsLoaded("spacechase0.GenericModConfigMenu") && apiManager.HookIntoGenericModConfigMenu(Helper))
+            {
+                var configApi = apiManager.GetGenericModConfigMenuApi();
+                configApi.Register(ModManifest, () => modConfig = new ModConfig(), () => Helper.WriteConfig(modConfig));
+
+
+                configApi.AddSectionTitle(ModManifest, I18n.Config_BetterBuildMenu_Title);
+            }
+        }
+    }
+}
